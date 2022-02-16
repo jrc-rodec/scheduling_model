123f1a1d9d50311965db17e8f99a691d6b7489a7
dhutter92 <david.hutter@fhv.at>
2022-02-16 13:36:25 +0100

    name change in optimizer

--- code/optimizer.py
+++ code/optimizer.py
@@ -5,7 +5,7 @@ class Optimizer:
     def __init__(self):
         pass
 
-    def optimize(self, assignments, jobs, system_info, last_timeslot):
+    def optimize(self, assignments, jobs, simulation_environment, last_timeslot):
         pass
 
 class Randomizer(Optimizer):
@@ -14,11 +14,11 @@ class Randomizer(Optimizer):
         super().__init__()
         self.name = "Randomizer"
     
-    def optimize(self, assignments, jobs, system_info, last_timeslot):
+    def optimize(self, assignments, jobs, simulation_environment, last_timeslot):
         for i in range(len(assignments)):
             job = jobs[i]
             assignment = assignments[i]
-            workstations = system_info.get_valid_workstations(job.task_id)
+            workstations = simulation_environment.get_valid_workstations(job.task_id)
             if len(workstations) == 0:
                 print(job.task_id)
             assignment[0] = workstations[random.randint(0, len(workstations) - 1)].external_id
