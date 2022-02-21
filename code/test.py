eb1118efb6b6c012970594f86abd972693ee2a7a
dhutter92 <david.hutter@fhv.at>
2022-02-21 16:00:23 +0100

    added GA (no feasibility checks)

--- code/test.py
+++ code/test.py
@@ -1,5 +1,6 @@
+from math import comb
 from external_test_data.read_data import read_dataset_1
-
+import random
 def get_amount_operations_for_job(index : int, jobs) -> int:
     return len(jobs[index])
 
@@ -8,9 +9,10 @@ def get_available_combinations_for_operation(operation_index : int, job_index : 
 
 def get_available_machines_for_operation(operation_index : int, job_index : int, jobs):
     machines = []
-    for operation in jobs[job_index][operation_index]:
-        if operation[0] not in machines:
-            machines.append[operation[0]]
+    for combinations in jobs[job_index][operation_index]:
+        for combination in combinations:
+            if combination[0] not in machines:
+                machines.append(combination[0])
     return machines
 
 def get_available_workers_for_operation(operation_index : int, job_index : int, jobs):
@@ -22,10 +24,11 @@ def get_available_workers_for_operation(operation_index : int, job_index : int, 
 
 def get_available_worker_for_machine_for_operation(machine_id : int, operation_index : int, job_index : int, jobs):
     workers = []
-    for operation in jobs[job_index][operation_index]:
-        if operation[0] == machine_id:
-            if operation[1] not in workers:
-                workers.append(operation[1])
+    for combinations in jobs[job_index][operation_index]:
+        for combination in combinations:
+            if combination[0] == machine_id:
+                if combination[1] not in workers:
+                    workers.append(combination[1])
     return workers
 
 def get_duration(machine_id : int, worker_id : int, operation_index : int, job_index : int, jobs):
@@ -33,6 +36,155 @@ def get_duration(machine_id : int, worker_id : int, operation_index : int, job_i
         if operation[0] == machine_id and operation[1] == worker_id:
             return operation[2]
     return 0
+
+def map_index_to_operation(index, orders, jobs):
+    current = 0
+    # operation_amount = 0
+    if index == 0:
+        return 0, orders[0]
+    while current < index:
+        for order in orders:
+            operation_amount = get_amount_operations_for_job(order[0], jobs)
+            if current + operation_amount > index:
+                # found correct order
+                operation_id = current + operation_amount - index
+                # found the id of the operation inside the job
+                return operation_id - 1, order
+            current += operation_amount
+    return -1, -1
+
+class Individual:
+
+    def __init__(self, genes, fitness):
+        self.genes = genes
+        self.fitness = fitness
+
+    def set_gene(self, index, gene):
+        self.genes[index] = gene
+    
+    def get_gene(self, index):
+        return self.genes[index]
+    
+    def is_feasible(self):
+        return True
+
+class SimpleGA:
+    
+    def __init__(self):
+        self.current_best = Individual([], float('inf'))
+
+    def randomize_gene(self, individual, index, operation_id, order):
+        individual.genes[index][0] = random.choice(get_available_machines_for_operation(operation_id, order[0], self.jobs))
+        individual.genes[index][1] = random.choice(get_available_worker_for_machine_for_operation(individual.genes[index][0], operation_id, order[0], self.jobs))
+        individual.genes[index][2] = random.randint(self.earliest_slot, self.last_slot)
+
+    def randomize_individual(self, individual, orders, jobs):
+            j = 0
+            for order in orders:
+                operations = get_amount_operations_for_job(order[0], jobs)
+                for k in range(operations):
+                    self.randomize_gene(individual, j, k, order)
+                    j+=1
+            return individual
+
+    def evaluate(self, individuals):
+        for individual in individuals:
+            if individual.is_feasible():
+                # calc fitness
+                individual.fitness = 0
+                for i in range(len(individual.genes)):
+                    operation_id, order = map_index_to_operation(i, self.orders, self.jobs)
+                    # order = self.orders[order_id]
+                    if not order == -1:
+                        delivery_date = order[1]
+                        duration = get_duration(individual.genes[i][0], individual.genes[i][1], operation_id, order[0], self.jobs)
+                        if individual.genes[i][2] + duration > delivery_date: # counts all late operations, not just late order once
+                            individual.fitness += 1
+                    else:
+                        individual.fitness = float('inf')
+            else:
+                individual.fitness = float('inf')
+
+    def crossover(self, parents):
+        # select parents (just random for now)
+        parent1 = random.choice(parents)
+        parent2 = random.choice(parents)
+        while parent1 == parent2:
+            parent2 = random.choice(parents)
+        # simple one point crossover for testing
+        crossover_point = random.randint(0, len(parent1.genes))
+        child1 = Individual(parent1.genes.copy(), float('inf'))
+        child2 = Individual(parent2.genes.copy(), float('inf'))
+        for i in range(crossover_point, len(parent1.genes)):
+            child1.set_gene(i, parent2.get_gene(i))
+            child2.set_gene(i, parent1.get_gene(i))
+        return child1, child2
+
+    def run(self, input, orders, system_info, jobs, max_generations, population_size, offspring_amount, earliest_slot, last_slot):
+        population = []
+        offsprings =  []
+        self.earliest_slot = earliest_slot
+        self.last_slot = last_slot
+        self.orders = orders
+        self.jobs = jobs
+        for i in range(population_size):
+            individual = input.copy()
+            x = Individual(individual, float('inf'))
+            self.randomize_individual(x, orders, jobs)
+            population.append(x)
+        self.evaluate(population)
+        for parent in population:
+            if parent.fitness < self.current_best.fitness:
+                self.current_best = parent
+        p = 1 / len(input)
+        gen = 0
+        
+        while gen < max_generations and self.current_best.fitness > 0:
+            print(f'Current generation: {gen}, Current Best: {self.current_best.fitness}')
+            # create offsprings
+            offsprings = []
+            # crossover
+            i = 0
+            while i < offspring_amount:
+                offspring1, offspring2 = self.crossover(population)
+                offsprings.append(offspring1)
+                i+=1
+                if len(offsprings) + 1 < offspring_amount:
+                    offsprings.append(offspring2) # discard offspring 2 if too many offsprings were created
+                    i += 1
+            # mutate
+            for offspring in offsprings:
+                i = 0
+                for i in range(len(offspring.genes)):
+                    if random.uniform(0, 1) < p:
+                        operation_id, order = map_index_to_operation(i, orders, jobs)
+                        self.randomize_gene(offspring, i, operation_id, order)
+                    i += 1
+            # evaluate
+            self.evaluate(offsprings)
+            # select new population
+            all = population + offsprings # use elitism
+            all.sort(key=lambda x: x.fitness)
+            population = all[0:population_size]
+            if population[0].fitness < self.current_best.fitness:
+                self.current_best = population[0]
+            gen += 1
+        return self.current_best
+
+def print_instance(instance):
+    print(f'System Information:')
+    system_info = instance[0]
+    print(f'There should be {system_info[0]} jobs, {system_info[1]} machines, and {system_info[2]} workers in the system')
+    jobs = instance[1]
+    print(f'Found {len(jobs)} different jobs')
+    i = 0
+    for job in jobs:
+        print(f'Job {i} has {len(job)} operations')
+        j = 0
+        for operation in job:
+            print(f'Operation {j} in Job {i} has {len(operation)} possible combinations')
+            j += 1
+        i += 1
 
 # input structure:
 # for each order
@@ -44,11 +196,18 @@ def get_duration(machine_id : int, worker_id : int, operation_index : int, job_i
 #   startime + duration of machine m with worker w for operation n < order o delivery date
 #   no overlap of timeslots for each machine and each worker
 # in the given dataset, all jobs seem to always have exactly the same amount of operations necessary
-input, orders, instance = read_dataset_1(use_instance=13)
-print(orders)
+earliest_slot = 500
+last_slot = 10000
+input, orders, instance = read_dataset_1(use_instance=13, earliest_time=earliest_slot, planning_horizon=last_slot)
+# print(orders)
 system_info = instance[0]
 jobs = instance[1]
 n_jobs = system_info[0]
 n_machines = system_info[1]
 n_workers = system_info[2]
+# print_instance(instance)
 # ready to start optimization
+
+ga = SimpleGA()
+result = ga.run(input, orders, system_info, jobs, 500, 50, 70, earliest_slot, last_slot)
+print(result.fitness)
