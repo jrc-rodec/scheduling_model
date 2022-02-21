cabc4ff6ce7c45e4b843de87b139e9314339f212
dhutter92 <david.hutter@fhv.at>
2022-02-21 10:48:52 +0100

    adapted earliest time + planning horizon

--- code/external_test_data/read_data.py
+++ code/external_test_data/read_data.py
@@ -85,8 +85,8 @@ data = read(selected_source)
 use_instance = 13 # between 0 and 13 for example data 1
 instance = data[use_instance]
 order_amount = 100 # how many orders should be generated
-earliest_time = 50 # can not expect delivery before 50 time units into the schedule are done
-planning_horizon = 1000 # what is the last possible date for orders' delivery date
+earliest_time = 500 # can not expect delivery before 50 time units into the schedule are done
+planning_horizon = 10000 # what is the last possible date for orders' delivery date
 orders = generate_orders(instance, order_amount, earliest_time, planning_horizon)
 #print(instance[1][0]) # print operations of job 1
 #print(instance[1][0][0]) # print possibilities for operation 1 of the operations of job 1 (which machines (first index), which workers per machine (second index), how long (3.)))
