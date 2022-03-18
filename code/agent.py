375dd51ab950ef04fbf4e736902ee6dc59da345a
huda <david.hutter@fhv.at>
2022-03-18 08:55:28 +0100

    added comment to is_blocked

--- code/agent.py
+++ code/agent.py
@@ -53,6 +53,7 @@ class GreedyAgent(Agent):
                     return True
                 if start_time + duration > assignment_start_time and start_time + duration < assignment_start_time + assignment_duration:
                     return True
+                # if new task is longer than already scheduled task, it could start before and end after -> overlap
                 if assignment_start_time > start_time and assignment_start_time < start_time + duration:
                     return True
                 if assignment_start_time + assignment_duration > start_time and assignment_start_time + assignment_duration < start_time + duration:
