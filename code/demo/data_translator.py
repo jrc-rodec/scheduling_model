c4e1e8b4d27ad6bab169cc33f87506dc1960b388
dhutter-fhv <david.hutter@fhv.at>
2022-10-14 13:50:46 +0200

    example code

--- code/demo/data_translator.py
+++ code/demo/data_translator.py
@@ -1,2 +1,21 @@
 class DataTranslator:
     pass
+
+class TestTranslator(DataTranslator):
+
+    def translate(self, n_workstations, recipies, processing_times):
+        workstations =  []
+        recipies = []
+        tasks =  []
+        for _ in range(len(n_workstations)):
+            #workstations.append(Workstation(...))
+            pass
+        for recipy in recipies:
+            tasks_for_recipe = []
+            pass
+            for i in range(recipy):
+                for duration in processing_times[i]:
+                    # create task + add to workstation
+                    #workstations[i] ...
+                    pass
+            # add recipies with all tasks
