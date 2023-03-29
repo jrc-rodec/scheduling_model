9d5aecee435c145af438174004023a083df97733
dhutter92 <david.hutter@fhv.at>
2023-03-29 16:45:56 +0200

    added test code

--- code/reworked_data_model/test.py
+++ code/reworked_data_model/test.py
@@ -1,5 +1,8 @@
-from data_translator import BenchmarkTranslator
+from data_translator import BenchmarkTranslator, TimeWindowGAEncoder
 
 benchmark_translator = BenchmarkTranslator()
 production_environment = benchmark_translator.translate(7)
-print('just as breakpoint')
+encoder = TimeWindowGAEncoder()
+orders = []
+values, jobs = encoder.encode(production_environment, orders)
+print(values)
