0f5db4d408c1307bc4704d888d4a6925b18ced10
dhutter92 <david.hutter@fhv.at>
2023-03-30 16:45:55 +0200

    added ga test implementation, basic ga code for time window scheduling

---
+++ code/experiments/ga_test_implementation.py
@@ -0,0 +1,149 @@
+import random
+from copy import deepcopy
+
+class GeneticAlgorithm:
+    
+    def __init__(self, dimensions : int = 10, lower_bounds : list[int] = [], upper_bounds : list[int] = [], population_size : int = 25, offspring_amount : int = 50, elitism : bool = False, mutation_probability : float = None, max_generations : int = 100) -> None:
+        self.max_generations = max_generations
+        self.population_size = population_size
+        self.offspring_amount = offspring_amount
+        self.dimensions = dimensions
+        self.elitism = elitism
+        if not mutation_probability:
+            mutation_probability = 1 / dimensions
+        self.mutation_probability = mutation_probability
+        self.lower_bounds = lower_bounds
+        self.upper_bounds = upper_bounds
+
+    def initialize_population(self) -> list[list[int]]:
+        population : list[list[int]] = []
+        for i in range(self.population_size):
+            population.append([])
+            for j in range(self.dimensions):
+                population[i].append(random.randint(self.lower_bounds[j], self.upper_bounds[j]+1))
+        return population
+
+    def _is_feasible(self, individual : list[int]) -> bool:
+        return True
+
+    def _evaluate(self, individual : list[int]) -> list[float]:
+        sum = 0
+        for value in individual:
+            sum += value
+        return sum
+
+    def _evaluate_population(self, population : list[list[int]]) -> list[list[float]]:
+        population_fitness : list[list[float]] = []
+        for individual in population:
+            objective_values = self._evaluate(individual)
+            population_fitness.append(objective_values)
+        return population_fitness
+
+    def _select(self, population, probabilities):
+        r = random.random()
+        for i in range(len(probabilities)):
+            if r < probabilities[i]:
+                return population[i]
+        return population[-1]
+    
+    def _select_next_generation(self, pool : list[list[int]], fitness) -> list[list[int]]:
+        probabilities : list[float] = []
+        sum = 0
+        for f in fitness:
+            sum += f
+        population = []
+        previous_probability = 0.0
+        while len(population) < self.population_size:
+            for i in range(len(pool)):
+                probability = previous_probability + (fitness[i] / sum)
+                probabilities.append(probability)
+                previous_probability = probability
+            selection = self._select(pool, probabilities)
+            probabilities.clear()
+            population.append(selection)
+            idx = pool.index(selection)
+            pool.pop(idx)
+            fitness.pop(idx)
+            """del pool[idx]
+            del fitness[idx]"""
+        return deepcopy(population), deepcopy(fitness)
+
+    def _select_parents(self, population : list[list[int]], population_fitness : list[list[float]]) -> tuple[list[int], list[int]]:
+        sum = 0
+        for fitness in population_fitness:
+            sum += fitness
+        probabilities : list[float] = []
+        previous_probability = 0.0
+        for i in range(len(population)):
+            probability = previous_probability + (population_fitness[i] / sum)
+            probabilities.append(probability)
+            previous_probability = probability
+        return self._select(population, probabilities), self._select(population, probabilities) # just for testing
+
+    def _mutate(self, individual : list[int]) -> None:
+        for i in range(len(individual)):
+            if random.random() < self.mutation_probability:
+                individual[i] = random.randint(self.lower_bounds[i], self.upper_bounds[i]+1)
+
+    def _recombine(self, parent_a : list[int], parent_b : list[int]) -> tuple[list[int], list[int]]:
+        crossover_point_a = random.randint(0, len(parent_a)-1)
+        #crossover_point_b = random.randint(crossover_point_a, len(parent_a))
+        offspring_a : list[int] = []
+        offspring_b : list[int] = []
+        offspring_a = parent_a[:crossover_point_a]
+        offspring_a.extend(parent_b[crossover_point_a:])
+        offspring_b = parent_b[:crossover_point_a]
+        offspring_b.extend(parent_a[crossover_point_a:])
+        return offspring_a, offspring_b
+
+    def solve(self):
+        population : list[list[int]] = self.initialize_population()
+        population_fitness : list[list[float]] = self._evaluate_population(population)
+        offsprings : list[list[int]] = []
+        offspring_fitness : list[list[float]] = []
+        best = (population[0], population_fitness[0])
+        best_fitness_history = []
+        average_fitness_history = []
+        for generation in range(self.max_generations):
+            offsprings.clear()
+            offspring_fitness.clear()
+            best_fitness_history.append(best[1])
+            average_fitness_of_generation = 0
+            for fitness in population_fitness:
+                average_fitness_of_generation += fitness
+            average_fitness_history.append(average_fitness_of_generation / len(population))
+            # recombine
+            while len(offsprings) < self.offspring_amount:
+                parent_a, parent_b = self._select_parents(population, population_fitness)
+                offspring_a, offspring_b = self._recombine(parent_a, parent_b)
+                # mutate
+                self._mutate(offspring_a)
+                offsprings.append(offspring_a)
+                if len(offsprings) + 1 <= self.offspring_amount:
+                    self._mutate(offspring_b)
+                    offsprings.append(offspring_b)
+            # evaluate offsprings
+            offspring_fitness = self._evaluate_population(offsprings)
+            for i in range(len(offspring_fitness)):
+                if offspring_fitness[i] < best[1]:
+                    best = (offsprings[i], offspring_fitness[i])
+            # select next generation
+            if self.elitism:
+                offsprings.extend(population)
+                offspring_fitness.extend(population_fitness)
+            population.clear()
+            population_fitness.clear()
+            # ...
+            population, population_fitness = self._select_next_generation(offsprings, offspring_fitness)
+            
+        return best, best_fitness_history, average_fitness_history
+    
+ga = GeneticAlgorithm(10, [0] * 10, [5] * 10, 50, 100, False, 1/10, 500)
+result, best_fitness_history, average_fitness_history = ga.solve()
+print(result)
+
+import matplotlib.pyplot as plt
+
+plt.plot(best_fitness_history)
+plt.plot(average_fitness_history)
+plt.show()
--- code/reworked_data_model/solver.py
+++ code/reworked_data_model/solver.py
@@ -1,5 +1,6 @@
 from model import ProductionEnvironment, Job, Order
 from copy import deepcopy
+import random
 
 class Solver:
     pass
@@ -22,28 +23,121 @@ class TimeWindowGASolver(Solver):
             mutation_probability = 1 / len(encoding)
         self.mutation_probability = mutation_probability
 
-    def initialize_population(self) -> list[list[int]]:
+    def initialize_population(self) -> list[list[int]]: # TODO
         pass
 
-    def _is_feasible(self, individual : list[int]) -> bool:
+    def _is_feasible(self, individual : list[int]) -> bool: # TODO
         pass
 
-    def _evaluate(self, individual : list[int]) -> list[float]:
+    def _evaluate(self, individual : list[int]) -> list[float]: # TODO
         pass
 
-    def _select_parents(self, population : list[list[int]]) -> tuple[list[int], list[int]]:
-        pass
+    def _evaluate_population(self, population : list[list[int]]) -> list[list[float]]:
+        population_fitness : list[list[float]] = []
+        for individual in population:
+            objective_values = self._evaluate(individual)
+            population_fitness.append(objective_values)
+        return population_fitness
 
-    def _mutate(self, individual : list[int]) -> list[int]:
+    def _get_weighted_probabilities(self, population : list[list[int]], population_fitness : list[list[float]]) -> list[float]:
+        probabilities : list[float] = []
+        sum = 0
+        for fitness in population_fitness:
+            sum += fitness[0] #NOTE/TODO: only one objective value for now
+        population = []
+        previous_probability = 0.0
+        while len(population) < self.population_size:
+            for i in range(len(population)):
+                probability = previous_probability + (fitness[i] / sum)
+                probabilities.append(probability)
+                previous_probability = probability
+        return probabilities
+
+    def _roulette_wheel_selection(self, population : list[list[int]], population_fitness : list[list[float]]):
+        r = random.random()
+        probabilities = self._get_weighted_probabilities(population, population_fitness)
+        for i in range(len(probabilities)):
+            if r < probabilities[i]:
+                return population[i]
+        return population[-1]
+
+    def _select_parents(self, population : list[list[int]], population_fitness : list[list[float]]) -> tuple[list[int], list[int]]:
+        parent_a = self._roulette_wheel_selection(population, population_fitness)
+        parent_b = []
+        while parent_a == parent_b: # force 2 different parents
+            parent_b = self._roulette_wheel_selection(population, population_fitness)
+        return parent_a, parent_b
+
+    def _mutate(self, individual : list[int]) -> list[int]: # TODO
         pass
 
     def _recombine(self, parent_a : list[int], parent_b : list[int]) -> tuple[list[int], list[int]]:
-        pass
+        crossover_point_a = random.randint(0, len(parent_a) - 2)
+        crossover_point_b = random.randint(crossover_point_a + 1, len(parent_a) - 1)
+        offspring_a = []
+        offspring_b = []
+        for i in range(crossover_point_a):
+            offspring_a.append(parent_a[i])
+            offspring_b.append(parent_b[i])
+        for i in range(crossover_point_a + 1, crossover_point_b):
+            offspring_a.append(parent_b[i])
+            offspring_b.append(parent_a[i])
+        for i in range(crossover_point_b + 1, len(parent_a)):
+            offspring_a.append(parent_a[i])
+            offspring_b.append(parent_b[i])
+        return deepcopy(offspring_a), deepcopy(offspring_b)
+    
+    def get_best(self, population : list[list[int]], population_fitness : list[list[float]]):
+        best : tuple[list[list[int]], list[list[float]]] = population[0]
+        for i in range(1, len(population_fitness)):
+            if population_fitness[i][0] < best[1][0]:
+                best = (population[i], population_fitness[i])
+        return deepcopy(best)
+
 
     def solve(self, jobs : list[Job]):
         self.jobs = jobs
         population : list[list[int]] = self.initialize_population()
+        population_fitness : list[list[float]] = self._evaluate_population(population)
         offsprings : list[list[int]] = []
+        offspring_fitness : list[list[float]] = []
+        best : tuple[list[int], list[float]] = self.get_best(population, population_fitness)
+        
         for generation in range(self.max_generations):
-            pass
-        pass
+            offsprings.clear()
+            offspring_fitness.clear()
+
+            # recombine
+            while len(offsprings < self.offspring_amount):
+                parent_a, parent_b = self._select_parents(population, population_fitness)
+                offspring_a, offspring_b = self._recombine(parent_a, parent_b)
+                # mutate
+                self._mutate(offspring_a)
+                offsprings.append(offspring_a)
+                if len(offsprings) + 1 <= self.offspring_amount:
+                    self._mutate(offspring_b)
+                    offsprings.append(offspring_b)
+
+            # evaluate offsprings
+            offspring_fitness = self._evaluate_population(offsprings)
+            
+            best_offspring = self.get_best(offsprings, offspring_fitness)
+            if best_offspring[1][0] < best[1][0]:
+                best = best_offspring
+
+            # select next generation
+            if self.elitism:
+                offsprings.extend(population)
+                offspring_fitness.extend(population_fitness)
+            
+            population.clear()
+            population_fitness.clear()
+
+            while len(population) < self.population_size:
+                individual : list[int] = self._roulette_wheel_selection(offsprings, offspring_fitness)
+                idx = offsprings.index(individual)
+                population.append(individual)
+                population_fitness.append(offspring_fitness[idx])
+                offsprings.pop(idx)
+                offspring_fitness.pop(idx)
+        return best
