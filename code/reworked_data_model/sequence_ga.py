8cd8cf697b893955944caeb8015035d05a99b6a1
dhutter92 <david.hutter@fhv.at>
2023-06-19 18:46:49 +0200

    testing

--- code/reworked_data_model/sequence_ga.py
+++ code/reworked_data_model/sequence_ga.py
@@ -341,17 +341,17 @@ def generate_one_order_per_recipe(production_environment : ProductionEnvironment
 
 encoder = TimeWindowSequenceEncoder()
 source = '6_Fattahi'
-benchmark_id = 16
+benchmark_id = 20
 production_environment : ProductionEnvironment = FJSSPInstancesTranslator().translate(source, benchmark_id)
 orders = generate_one_order_per_recipe(production_environment)
 solver = TimeWindowSequenceGA(production_environment, encoder)
 values, jobs = encoder.encode(production_environment, orders)
 solver.initialize(jobs)
 
-solver.max_generations = 10000
+solver.max_generations = 50000
 solver.add_objective(Makespan())
-population_size = 50
-offspring_amount = 100
+population_size = 75
+offspring_amount = 150
 
 solver.mutate_workers = False
 solver.mutate_duration = False
