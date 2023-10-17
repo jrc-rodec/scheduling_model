09a37de160b386733832869c2eff84da656387ad
dhutter-fhv <david.hutter@fhv.at>
2023-10-17 13:06:52 +0200

    testing with skipping

--- code/reworked_data_model/experiments.py
+++ code/reworked_data_model/experiments.py
@@ -177,7 +177,7 @@ def test_individual_adjustment(source, instance, max_generation : int = 5000, ti
 
 def save_adjustment_experiments(fitness, run_time, fevals, generations, restarts, source, instance, adjust):
     #file = 'C:/Users/huda/Documents/GitHub/scheduling_model/code/reworked_data_model/results/comparison.txt'
-    file = r'C:\Users\dhutt\Desktop\SCHEDULING_MODEL\code\reworked_data_model\results\local_minimum_testing.txt'
+    file = r'C:\Users\huda\Documents\GitHub\scheduling_model\code\reworked_data_model\results\local_minimum_testing.txt'
     #maybe add values to dict and use dict writer
     with open(file, 'a') as f:
         f.write(f'{source};{instance};{run_time};{fevals};{generations};{restarts};{fitness};{adjust}\n')
@@ -305,7 +305,7 @@ if __name__ == '__main__':
     #source = '1_Brandimarte'
     #instance = 1
     #known_best = 40
-    selection = [('5_Kacem', 1, 11), ('6_Fattahi', 15, 514), ('1_Brandimarte', 1, 40)]#('5_Kacem', 4, 11)]#, ('6_Fattahi', 10, 516), ('6_Fattahi', 15, 514), ('1_Brandimarte', 1, 40), ('1_Brandimarte', 11, 649), ('4_ChambersBarnes', 6, 927)]
+    selection = [('5_Kacem', 1, 11), ('4_ChambersBarnes', 6, 927), ('6_Fattahi', 15, 514), ('1_Brandimarte', 1, 40)]#('5_Kacem', 4, 11)]#, ('6_Fattahi', 10, 516), ('6_Fattahi', 15, 514), ('1_Brandimarte', 1, 40), ('1_Brandimarte', 11, 649), ('4_ChambersBarnes', 6, 927)]
     time_limit = 900
     #selection = selection[6:]
     #for benchmark_source in sources:
