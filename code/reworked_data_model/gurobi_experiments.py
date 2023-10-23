ccc39562952cbb309e1af64c65641a3ac316f16b
dhutter-fhv <david.hutter@fhv.at>
2023-10-23 10:17:05 +0200

    gurobi ready for experiments

--- code/reworked_data_model/gurobi_experiments.py
+++ code/reworked_data_model/gurobi_experiments.py
@@ -1,19 +1,27 @@
 from solver import GurobiSolver
 from translation import GurobiEncoder, FJSSPInstancesTranslator
 from model import Order
+import time
+import os
+import inspect
 
-def write_result(source : str, instance : int, expected_result, runtime : float, optimal : bool, fitness : float, gap : float):
+#upper bound, lower bound, best solution (fitness),gap, status, # explored solution, laufzeit, gesamtlaufzeit, X+Y+C vektoren
+def write_result(source : str, instance : int, best_result, upper_bound : float, lower_bound : float, x, y, c, gap, status, n_explored_solutions, runtime, overall_runtime):
     path = r'C:\Users\huda\Documents\GitHub\scheduling_model\code\reworked_data_model\results\gurobi_experiment_results.txt'
     with open(path, 'a') as f:
-        f.write(f'{source};{instance};{runtime};{optimal};{expected_result};{fitness}\n')
+        f.write(f'{source};{instance};{best_result};{upper_bound};{lower_bound};{gap};{status};{n_explored_solutions};{runtime};{overall_runtime};{x};{y};{c}\n')
 
-experiments = 1
-instances = [['5_Kacem', 1, 11], ['5_Kacem', 4, 12]]
-for instance in instances:
-    for i in range(experiments):
-        source = instance[0]#'5_Kacem'
-        benchmark_id = instance[1]#4
-        best_known = instance[2] # 0 if unknown
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+read_path = currentdir + '/../external_test_data/FJSSPinstances/'
+write_path = currentdir + '/changed_benchmarks/'
+use_sources = ['0_BehnkeGeiger', '1_Brandimarte', '2a_Hurink_sdata', '2b_Hurink_edata', '2c_Hurink_rdata','2d_Hurink_vdata', '3_DPpaulli', '4_ChambersBarnes', '5_Kacem', '6_Fattahi']
+write_path = currentdir + '/benchmarks_with_workers/'
+for benchmark_source in use_sources:
+    full_path = read_path + benchmark_source + '/'
+    for i in range(len(os.listdir(full_path))):
+        source = benchmark_source
+        benchmark_id = i+1
 
         simple_translator = FJSSPInstancesTranslator()
         production_environment = simple_translator.translate(source=source, benchmark_id=benchmark_id)
@@ -24,35 +32,11 @@ for instance in instances:
 
         encoder = GurobiEncoder()
         nb_machines, nb_jobs, nb_operations, job_ops_machs, durations, job_op_suitable, upper_bound, jobs = encoder.encode(production_environment, orders)
+        start_time = time.time()
         solver = GurobiSolver(production_environment)
         solver.initialize(nb_jobs, nb_operations, nb_machines, job_ops_machs, durations, job_op_suitable, upper_bound)
-        solver.m.Params.TIME_LIMIT = 20 # time limit in seconds
+        solver.m.Params.TIME_LIMIT = 1800 # time limit in seconds -> 30 Minutes - 1800
         solver.run()
-
-        #if solver.m.Status == 2:
-        #    # Optimum found
-        #    print(solver.m.ObjVal)
-        #elif solver.m.Status == 9:
-        #    # Time Limit Exceeded
-        #    print(f'Bounds: {solver.m.ObjBound}')
-        #NOTE: Gurobi does not count initialization (including creating all constraints) for the time limit 
-        #print(solver.get_best_fitness())
-        #print(f'Optimal: {solver.m.Status == 2}') # NOTE: status 2 = optimal, status 9 = time limit reached
-        #print(f'Time Limit Reached: {solver.m.Status == 9}')
-        #xsol, ysol, csol = solver.get_best() # TODO: change signature
-        #print(f'XSOL: {xsol}')
-        #print(f'YSOL: {ysol}')
-        #print(f'CSOL: {csol}')
-        #print(f'Runtime: {solver.m.Runtime}')
-        #print(f'Gap: {solver.m.MIPGap}')
-        #values = solver.m.getVars()
-        #print(solver.Cmax.X)
-        print(solver.Cmax.UB)
-        print(solver.Cmax.X)
-        print(solver.m.Xn)
-        print(f'MAX BOUND: {solver.m.MaxBound}')
-        print(f'MAX COEFF: {solver.m.MaxCoeff}')
-        print(f'MIN COEFF: {solver.m.MinCoeff}')
-        print(f'POOL OBJ BOUND: {solver.m.PoolObjBound}')
-        #solver.m.write(r'C:\Users\huda\Documents\GitHub\scheduling_model\code\reworked_data_model\results\gurobi_results.sol')
-        write_result(instance[0], instance[1], instance[2], solver.m.Runtime, solver.m.Status == 2, solver.Cmax.X, solver.m.MIPGap) # TODO: best feasible fitness
+        real_time = time.time() - start_time
+        xsol, ysol, csol = solver.get_best() 
+        write_result(source, benchmark_id, solver.m.objVal, solver.Cmax.UB, solver.Cmax.LB, xsol, ysol, csol, solver.m.MIPGap, solver.m.Status, solver.m.NodeCount, solver.m.Runtime, real_time)
