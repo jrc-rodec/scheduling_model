fcc022e02a209ff891c0b6ad930cf8eac4701129
dhutter92 <david.hutter@fhv.at>
2023-12-19 14:08:08 +0100

    local testing

--- code/reworked_data_model/experiments.py
+++ code/reworked_data_model/experiments.py
@@ -48,6 +48,40 @@ def run_experiment(source, instance, parameters : dict):
     run_time = time.time() - start_time
     return result, run_time, ga.function_evaluations, ga.restarts, ga.generations
 
+def run_experiment_history(source, instance, parameters : dict):
+    production_environment = FJSSPInstancesTranslator().translate(source, instance)
+    orders = generate_one_order_per_recipe(production_environment)
+    production_environment.orders = orders
+    workstations_per_operation, base_durations, job_operations = SequenceGAEncoder().encode(production_environment, orders)
+    ga = GA(job_operations, workstations_per_operation, base_durations)
+    population_size = parameters['population_size'] if 'population_size' in parameters else 100
+    offspring_amount = parameters['offspring_amount'] if 'offspring_amount' in parameters else population_size
+    max_generations = parameters['max_generations'] if 'max_generations' in parameters else None
+    run_for = parameters['time_limit'] if 'time_limit' in parameters else None
+    stop_at = parameters['target_fitness'] if 'target_fitness' in parameters else None
+    random_initialization = parameters['random_initialization'] if 'random_initialization' in parameters else False
+    elitism = parameters['elitism'] if 'elitism' in parameters else 0
+    allow_duplicate_parents = parameters['duplicate_parents'] if 'duplicate_parents' in parameters else False
+    pruning = parameters['pruning'] if 'pruning' in parameters else False
+    fill_gaps = parameters['fill_gaps'] if 'fill_gaps' in parameters else False
+    adjust_individuals = parameters['adjust_individuals'] if 'adjust_individuals' in parameters else False
+    adjust_parameters = parameters['adjust_mutation'] if 'adjust_mutation' in parameters else False
+    update_interval = parameters['mutation_update_interval'] if adjust_parameters else 0
+    p_increase_rate = parameters['mutation_increase_rate'] if adjust_parameters else 0
+    max_p = parameters['max_mutation_rate'] if adjust_parameters else 0
+    restart_at_max_p = parameters['restart_at_max_mutation_rate'] if adjust_parameters else False
+    avoid_local_mins = parameters['avoid_local_mins'] if adjust_parameters else False
+    local_min_distance = parameters['local_min_distance'] if avoid_local_mins else 0.0
+    sequence_mutation = parameters['sequence_mutation'] if 'sequence_mutation' in parameters else 'mix'
+    selection = parameters['selection'] if 'selection' in parameters else 'tournament'
+    tournament_size = parameters['tournament_size'] if selection == 'tournament' else 0
+    random_individual_per_generation_amount = parameters['random_individuals'] if 'random_individuals' in parameters else 0
+    output_interval = parameters['output_interval'] if 'output_interval' in parameters else 1000
+    start_time = time.time()
+    result, history = ga.run(population_size, offspring_amount, max_generations, run_for, stop_at, None, selection, tournament_size, adjust_parameters, update_interval=update_interval, p_increase_rate=p_increase_rate, max_p=max_p, restart_at_max_p=restart_at_max_p, avoid_local_mins=avoid_local_mins, local_min_distance=local_min_distance, elitism=elitism, sequence_mutation=sequence_mutation, pruning=pruning, fill_gaps=fill_gaps, adjust_optimized_individuals=adjust_individuals, random_individuals=random_individual_per_generation_amount, allow_duplicate_parents=allow_duplicate_parents, random_initialization=random_initialization, output_interval=output_interval)
+    run_time = time.time() - start_time
+    return result, history, run_time, ga.function_evaluations, ga.restarts, ga.generations
+
 def save_result(result, source, instance, run_time, parameters, fevals, restarts):
     file = 'C:/Users/huda/Documents/GitHub/scheduling_model/code/reworked_data_model/results/testing.txt'
     #maybe add values to dict and use dict writer
@@ -323,6 +357,42 @@ def ud_experiment(source, instance, max_generation : int = 5000, time_limit : in
     print(f'RESULT FOR {source}-{instance}:')
     print(result)
 
+def fattahi(source, instance, target_fitness):
+    parameters = {
+        'population_size': 5,
+        'offspring_amount': 20,
+        'max_generations': None,
+        'time_limit': None,
+        'target_fitness': target_fitness,
+        'elitism': 1,
+        'random_initialization': False,
+        'duplicate_parents': False,
+        'pruning': False,
+        'fill_gaps': True,
+        'adjust_individuals': True,
+        'adjust_mutation': True,
+        'mutation_update_interval': 100,
+        'mutation_increase_rate': 1.1,
+        'max_mutation_rate': 1.0,
+        'restart_at_max_mutation_rate': True,
+        'avoid_local_mins': False,
+        'local_min_distance': 0.1,
+        'sequence_mutation': 'swap',
+        'selection': 'tournament', # 'tournament'
+        'tournament_size': 2,
+        'random_individuals': 0,
+        'output_interval': 1000
+    }
+
+    result, history, run_time, function_evaluations, restarts, generations = run_experiment_history(source, instance, parameters)
+    save_fattahi_result(source, instance, result, history, run_time, function_evaluations, restarts, generations)
+
+def save_fattahi_result(source, instance, solution, history, run_time, function_evaluations, restarts, generations):
+    file = r'C:\Users\dhutt\Desktop\SCHEDULING_MODEL\code\reworked_data_model\results\fattahi\fattahi.txt'
+    #maybe add values to dict and use dict writer
+    with open(file, 'a') as f:
+        f.write(f'{source};{instance};{run_time};{function_evaluations};{generations};{restarts};{solution.fitness};{solution.sequence};{solution.workstations};{solution.durations};{history}\n')
+
 if __name__ == '__main__':
     currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
     read_path = currentdir + '/../external_test_data/FJSSPinstances/'
@@ -332,25 +402,27 @@ if __name__ == '__main__':
     #known_best = [66, 107, 221, 355, 119, 320, 397, 253, 210, 516, 468, 446, 466, 554, 514, 608, 879, 894, 1070, 1196]
     #known_best = [66, 107, 221, 355, 119, 320, 397, 253, 210, 516, 468, 446, 466, 0, 514, 0, 0, 0, 0, 0]
     #known_best = [11, 11, 7, 11]
-    n_experiments = 5
+    n_experiments = 1
     scores = []
     #source = '1_Brandimarte'
     #instance = 1
     #known_best = 40
     selection =  [('5_Kacem', 4, 12), ('6_Fattahi', 20, 1200)]#[('3_DPpaulli', 1, 0), ('3_DPpaulli', 5, 0)]#[('0_BehnkeGeiger', 60, 0)]#[('4_ChambersBarnes', 6, 927)]#('5_Kacem', 1, 11), ('4_ChambersBarnes', 6, 927), ('6_Fattahi', 15, 514), ('1_Brandimarte', 1, 40)]#('5_Kacem', 4, 11)]#, ('6_Fattahi', 10, 516), ('6_Fattahi', 15, 514), ('1_Brandimarte', 1, 40), ('1_Brandimarte', 11, 649), ('4_ChambersBarnes', 6, 927)]
-    time_limit = 3600
+    time_limit = None
     #selection = selection[6:]
     #for benchmark_source in sources:
     #full_path = read_path + source + '/'
+    selection = [('6_Fattahi', 20, 1196), ('6_Fattahi', 19, 1040)]
     for instance in selection:
         #if instance[0] != '5_Kacem':
         for j in range(n_experiments):
             #result, run_time, parameters = run(source, instance, max_generation=5000, time_limit=600, target_fitness=known_best[i], output=False)
             #result, run_time, parameters, fevals, restarts = run_lower_new_adaptation(source, instance, max_generation=None, time_limit=600, target_fitness=known_best, output=False)
-            ud_experiment(instance[0], instance[1], None, time_limit, instance[2])
+            #ud_experiment(instance[0], instance[1], None, time_limit, instance[2])
+            fattahi(instance[0], instance[1], instance[2])
             #kacem_and_brandimarte(instance[0], instance[1], True)
             #run_experiment_elitism(instance[0], instance[1], None, time_limit, instance[2], False, True)
-            print(f'{j} - {instance[0]}{instance[1]} - With Elitism')
+            print(f'{j} - {instance[0]}{instance[1]}')
         #for j in range(n_experiments):
             #result, run_time, parameters = run(source, instance, max_generation=5000, time_limit=600, target_fitness=known_best[i], output=False)
             #result, run_time, parameters, fevals, restarts = run_lower_new_adaptation(source, instance, max_generation=None, time_limit=600, target_fitness=known_best, output=False)
