492e92d2c73ae9ce4aaac39cb981f8252030f252
dhutter-fhv <david.hutter@fhv.at>
2024-02-05 13:57:48 +0100

    experiments + result visualization tests

--- code/upgrades/code/experiments.py
+++ code/upgrades/code/experiments.py
@@ -78,7 +78,7 @@ if __name__ == '__main__':
     read_path = currentdir + '/../benchmarks/'
 
     time_limit = 3600
-    n_experiments = 1
+    n_experiments = 10
     selection = [('5_Kacem', 2, 7)]
     histories : list[History] = []
     for instance in selection:
