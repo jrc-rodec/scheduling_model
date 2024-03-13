1d7cc7de32c27f8aa12982155c694e7bc395f372
dhutter92 <david.hutter@fhv.at>
2024-03-13 16:11:00 +0100

    TODO: check if results currently are always feasible

--- code/upgrades/CS_Translation/Solver/Program.cs
+++ code/upgrades/CS_Translation/Solver/Program.cs
@@ -6,13 +6,13 @@ namespace Solver
     {
         static void Main(string[] args)
         {
-            string path = "C:\\Users\\dhutt\\Desktop\\SCHEDULING_MODEL\\code\\upgrades\\code\\benchmarks\\6_Fattahi\\Fattahi20.fjs";
+            string path = "C:\\Users\\dhutt\\Desktop\\SCHEDULING_MODEL\\code\\upgrades\\code\\benchmarks\\0_BehnkeGeiger\\Behnke60.fjs";
             BenchmarkParser parser = new BenchmarkParser();
             Encoding result = parser.ParseBenchmark(path);
             DecisionVariables variables = new DecisionVariables(result);
             GAConfiguration configuration = new GAConfiguration(result, variables);
             GA ga = new GA(configuration);
-            List<Individual> best = ga.Run(100, 300, 0, 10000);
+            List<Individual> best = ga.Run(1000, 300, 0, 10000);
             Console.WriteLine(best[0].Fitness[Criteria.Makespan]);
         }
     }
