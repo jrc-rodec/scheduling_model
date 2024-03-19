5a51b5d43822b6033845e3dc3a86f844b4a3ea12
dhutter-fhv <david.hutter@fhv.at>
2024-03-19 12:05:32 +0100

    added preprocessor class

---
+++ code/upgrades/CS_Translation/PreProcessing/Preprocessor.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PreProcessing
+{
+    public class Preprocessor
+    {
+
+        public Preprocessor() { }
+
+        public void Process(int[,] durations, int[] jobSequence)
+        {
+            int nOperations = durations.GetLength(0);
+            int nMachines = durations.GetLength(1);
+            int nJobs = jobSequence.ToHashSet<int>().Count;
+            float flexibility;
+            float durationVariety;
+            float averageMachinesPerOperation;
+            float averageOperationsPerMachine;
+            int nPossibleCombinations = 0;
+            int[] onMachine = new int[nMachines];
+            HashSet<int> durationSet = new HashSet<int>();
+            for(int i = 0; i < durations.GetLength(0); ++i) 
+            { 
+                for(int j = 0; j < durations.GetLength(1); ++j)
+                {
+                    if (durations[i, j] > 0)
+                    {
+                        ++nPossibleCombinations;
+                        ++onMachine[j];
+                        durationSet.Add(durations[i, j]);
+                    }
+                }
+            }
+
+            float averageOperationsPerJob = (nOperations + 0.0f)/ nJobs;
+            averageOperationsPerMachine = (onMachine.Sum() + 0.0f) / onMachine.Length;
+            averageMachinesPerOperation = nPossibleCombinations / nOperations;
+            int nUniqueDurations = durationSet.Count;
+            durationVariety = (nUniqueDurations + 0.0f) / nPossibleCombinations;
+            flexibility = (averageMachinesPerOperation + 0.0f) / nMachines;
+            
+        }
+    }
+}
