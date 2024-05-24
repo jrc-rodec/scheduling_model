bc3d08e965113293ec04e2015c919a8a5e8f8217
bianca482 <biancakasper@gmx.at>
2024-05-24 11:33:48 +0200

    fixed len - shape problem

--- code/api_python/benchmarkparser/encoding.py
+++ code/api_python/benchmarkparser/encoding.py
@@ -13,10 +13,10 @@ class Encoding:
         return self.__job_sequence
     
     def n_operations(self):
-        return len(self.__durations[0])
+        return self.__durations.shape[0]
     
     def n_machines(self):
-        return len(self.__durations[1])
+        return self.__durations.shape[1]
     
     def n_jobs(self):
         return self.__n_jobs
@@ -26,16 +26,16 @@ class Encoding:
     
     def get_machines_for_operation(self, operation_index: int):
         machines = []
-        for i in range(0, len(self.__durations[1])): 
+        for i in range(0, self.__durations.shape[1]): 
             if self.__durations[operation_index, i] > 0:
                 machines.append(i)
         return machines
     
     def get_machines_for_all_operations(self):
         machines = []
-        for i in range (0, len(self.__durations[0])):
+        for i in range (0, self.__durations.shape[0]):
             machines.append([])
-            for j in range(0, len(self.__durations[1])):
+            for j in range(0, self.__durations.shape[1]):
                 if self.__durations[i, j] > 0: 
                     machines[i].append(j)
         return machines
@@ -44,9 +44,9 @@ class Encoding:
         return Encoding(self.__durations, self.__job_sequence)
     
     def deep_copy(self):
-        duration_copy = np.zeros((len(self.__durations[0]), len(self.__durations[1])), dtype=int)
-        for i in range(0, len(self.__durations[0])):
-            for j in range(0, len(self.__durations[1])):
+        duration_copy = np.zeros((self.__durations.shape[0], self.__durations.shape[1]), dtype=int)
+        for i in range(0, self.__durations.shape[0]):
+            for j in range(0, self.__durations.shape[1]):
                 duration_copy[i, j] = self.__durations[i, j]
         
         job_sequence_copy = [None] * len(self.__job_sequence)
@@ -68,10 +68,10 @@ class WorkerEncoding:
         return self.__job_sequence
     
     def n_operations(self):
-        return len(self.__durations[0])
+        return self.__durations.shape[0]
     
     def n_machines(self):
-        return len(self.__durations[1])
+        return self.__durations.shape[1]
     
     def n_jobs(self):
         return self.__n_jobs
@@ -81,30 +81,30 @@ class WorkerEncoding:
 
     def get_workers_for_operation(self, operation_index: int):
         workers = []
-        for i in range(0, len(self.__durations[1])): 
-            for j in range(0, len(self.__durations[2])):
+        for i in range(0, self.__durations.shape[1]): 
+            for j in range(0, self.__durations.shape[2]):
                 if self.__durations[operation_index, i, j] > 0:
-                    workers.append(i)
-                    break
+                    workers.append(j) # laut dem C# Code wird i hinzugefügt -> warum i? Sollte es nicht j für die Worker sein? 
 
         return workers
     
-    def get_machines_for_all_operations(self):
+    def get_all_machines_for_all_operations(self):
         operations = []
-        for i in range (0, len(self.__durations[0])):
+        for i in range (0, self.__durations.shape[0]):
             machines = []
-            for j in range(0, len(self.__durations[1])):
-                for k in range(0, len(self.__durations[2])):
+            for j in range(0, self.__durations.shape[1]):
+                for k in range(0, self.__durations.shape[2]):
                     if self.__durations[i, j, k] > 0: 
                         machines.append(j)
                         break
+                    
             operations.append(machines)
 
         return operations
     
     def get_workers_for_operation_on_machine(self, operation: int, machine: int):
         workers = []
-        for j in range(0, len(self.__durations[2])):
+        for j in range(0, self.__durations.shape[2]):
             if self.__durations[operation, machine, j] > 0:
                 workers.append(j)
 
@@ -117,10 +117,10 @@ class WorkerEncoding:
         return WorkerEncoding(self.__durations, self.__job_sequence)
     
     def deep_copy(self):
-        duration_copy = np.zeros((len(self.__durations[0]), len(self.__durations[1]), len(self.__durations[2])), dtype=int)
-        for i in range(0, len(self.__durations[0])):
-            for j in range(0, len(self.__durations[1])):
-                for k in range(0, len(self.__durations[2])):
+        duration_copy = np.zeros((self.__durations.shape[0], self.__durations.shape[1], self.__durations.shape[2]), dtype=int)
+        for i in range(0, self.__durations.shape[0]):
+            for j in range(0, self.__durations.shape[1]):
+                for k in range(0, self.__durations.shape[2]):
                     duration_copy[i, j, k] = self.__durations[i, j, k]
         
         job_sequence_copy = [None] * len(self.__job_sequence)
--- code/api_python/benchmarkparser/tests.py
+++ code/api_python/benchmarkparser/tests.py
@@ -6,8 +6,16 @@ durations = np.array([[1, 2, 0], [4, 0, 6], [0, 8, 9]])
 job_sequence = [1, 1, 2, 2, 3, 3, 3, 4]
 encoder = Encoding(durations, job_sequence)
 
-worker_durations = np.array([[[1, 0], [2, 1]], [[4, 3], [0, 0]], [[0, 1], [8, 9]]])
+worker_durations = np.array([
+    [[1, 0, 0], [0, 2, 3]],  
+    [[0, 4, 0], [5, 0, 6]],  
+    [[0, 0, 7], [8, 0, 0]]   
+])
 worker_encoder = WorkerEncoding(worker_durations, job_sequence)
+
+# The first dimension represents operations (3 operations in total).
+# The second dimension represents machines (2 machines per operation).
+# The third dimension represents workers (3 workers per machine).
 
 def test_encoder_n_jobs():
     assert encoder.n_jobs() == 4
@@ -16,10 +24,10 @@ def test_encoder_job_sequence():
     assert encoder.job_sequence() == job_sequence
     
 def test_encoder_n_operations():
-    assert encoder.n_operations() == len(durations[0])
+    assert encoder.n_operations() == durations.shape[0]
 
 def test_encoder_n_machines():
-    assert encoder.n_machines() == len(durations[1])
+    assert encoder.n_machines() == durations.shape[1]
 
 def test_encoder_get_machines_for_operation():
     assert encoder.get_machines_for_operation(2) == [1,2]
@@ -50,24 +58,23 @@ def test_worker_encoder_job_sequence():
     assert worker_encoder.job_sequence() == job_sequence
     
 def test_worker_encoder_n_operations():
-    assert worker_encoder.n_operations() == len(worker_durations[0])
+    assert worker_encoder.n_operations() == worker_durations.shape[0]
 
 def test_worker_encoder_n_machines():
-    assert worker_encoder.n_machines() == len(worker_durations[1])
+    assert worker_encoder.n_machines() == worker_durations.shape[1]
 
 def test_worker_encoder_get_workers_for_operation():
-    print(worker_encoder.get_workers_for_operation(2))
-    assert worker_encoder.get_workers_for_operation(2) == [1,2]
+    assert worker_encoder.get_workers_for_operation(1) == [1,0,2]
 
-def test_worker_encoder_get_machines_for_all_operations():
-    print(worker_encoder.get_machines_for_all_operations())
-    assert worker_encoder.get_machines_for_all_operations() == [[0], [0, 1], [0, 1], [1], [0, 1]]
+def test_worker_encoder_get_all_machines_for_all_operations():
+    assert worker_encoder.get_all_machines_for_all_operations() == [[0, 1], [0, 1], [0, 1]]
 
 def test_worker_encoder_get_workers_for_operation_on_machine():
-    assert worker_encoder.get_workers_for_operation_on_machine(2, 1) == [1,2]
+    assert worker_encoder.get_workers_for_operation_on_machine(1, 1) == [0, 2]
 
-def test_worker_is_possible():
-    assert worker_encoder.is_possible(2, 1, 1) == False
+def test_worker_encoder_is_possible():
+    assert worker_encoder.is_possible(0, 0, 0) == True
+    assert worker_encoder.is_possible(0, 0, 1) == False
 
 def test_worker_encoder_copy():
     copied = worker_encoder.copy()
@@ -126,8 +133,10 @@ def main():
     test_worker_encoder_job_sequence()
     test_worker_encoder_n_operations()
     test_worker_encoder_n_machines()
-    #test_worker_encoder_get_workers_for_operation()
-    #test_worker_encoder_get_machines_for_all_operations()
+    test_worker_encoder_get_workers_for_operation()
+    test_worker_encoder_get_all_machines_for_all_operations()
+    test_worker_encoder_get_workers_for_operation_on_machine()
+    test_worker_encoder_is_possible()
     test_worker_encoder_copy()
     test_worker_encoder_deep_copy()
 
