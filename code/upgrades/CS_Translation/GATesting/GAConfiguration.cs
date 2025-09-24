58c0e4406b21504f87746e7f05b51bcedd1a45f3
dhutter-fhv <david.hutter@fhv.at>
2025-09-24 11:28:53 +0200

    testing and logging prep

---
+++ code/upgrades/CS_Translation/GATesting/GAConfiguration.cs
@@ -0,0 +1,81 @@
+﻿using BenchmarkParsing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GATesting
+{
+    public class GAConfiguration
+    {
+
+        private readonly int[,,] _durations;
+        private readonly int[] _jobSequence;
+        private readonly int[] _jobStartIndices;
+        private readonly WorkerDecisionVariables _decisionVariables;
+
+        private int _populationSize = 5;
+        private int _offspringAmount = 20;
+        private float _mutationProbability;
+        private float _maxMutationProbability = 1.0f;//0.5f;//0.5f;//1.0f;
+        private float _elitismRate = 0.1f;
+        private int _tournamentSize = 1;
+        private float _populationGrowthRate = 2.0f;
+        private float _maxElitism = 0.1f; // 1.0f // scale freely between , and + if 1
+        private float _maxTournamentRate = 0.2f; // 1.0f // NOTE: 0 -> TournamentSize = 1 -> Random Selection, 1 -> TournamentSize = PopulationSize -> Rank Selection
+        private int _restartGenerations = 100;//1000;//50;
+
+        public GAConfiguration(WorkerEncoding encoding, WorkerDecisionVariables variables)
+        {
+            _durations = encoding.Durations;
+            _jobSequence = encoding.JobSequence;
+
+            _mutationProbability = 1.0f / (2.0f * _jobSequence.Length);
+            _decisionVariables = variables;
+
+            // NOTE: this should probably be moved somewhere else
+            Individual.Durations = _durations;
+            Individual.JobSequence = _jobSequence;
+            Individual.AvailableMachines = encoding.GetAllMachinesForAllOperations();
+            Individual.AvailableWorkers = encoding.GetAllWorkersForAllOperations(); // should work now
+            Individual.DetermineMaxDissimiarilty();
+
+            _jobStartIndices = new int[NJobs];
+            int operation = 0;
+            _jobStartIndices[operation++] = 0;
+            for (int i = 1; i < JobSequence.Length; ++i)
+            {
+                if (JobSequence[i] != JobSequence[i - 1])
+                {
+                    _jobStartIndices[operation++] = i;
+                }
+            }
+        }
+
+        public int[,,] Durations => _durations;
+        public int[] JobSequence => _jobSequence;
+        public float Flexibility => _decisionVariables.Flexibility;
+        public float AverageMachines => _decisionVariables.AverageMachines;
+        public float DurationVariety => _decisionVariables.DurationVariety;
+        public int NOperations => _decisionVariables.NOperations;
+        public int NMachines => _decisionVariables.NMachines;
+        public int NJobs => _decisionVariables.NJobs;
+        public float AverageOperations => _decisionVariables.AverageOperations;
+        public float AverageOperationsOnMachines => _decisionVariables.AverageOperationsOnMachines;
+        public float TheoreticalMinimumMakespan => _decisionVariables.TheoreticalMinimumMakespan;
+
+        public int PopulationSize { get => _populationSize; set => _populationSize = value; }
+        public int OffspringAmount { get => _offspringAmount; set => _offspringAmount = value; }
+        public float MutationProbability { get => _mutationProbability; set => _mutationProbability = value; }
+        public float ElitismRate { get => _elitismRate; set => _elitismRate = value; }
+        public int TournamentSize { get => _tournamentSize; set => _tournamentSize = value; }
+        public int RestartGenerations { get => _restartGenerations; set => _restartGenerations = value; }
+        public float MaxMutationProbability { get => _maxMutationProbability; set => _maxMutationProbability = value; }
+        public float PopulationSizeGrowthRate { get => _populationGrowthRate; set => _populationGrowthRate = value; }
+        public float MaxElitismRate { get => _maxElitism; set => _maxElitism = value; }
+        public float MaxTournamentRate { get => _maxTournamentRate; set => _maxTournamentRate = value; }
+
+        public int[] JobStartIndices => _jobStartIndices;
+    }
+}
---
+++ code/upgrades/CS_Translation/GATesting/Program.cs
@@ -0,0 +1,43 @@
+﻿using BenchmarkParsing;
+using System.Text.Json.Nodes;
+using static BenchmarkParsing.BenchmarkParser;
+
+namespace GATesting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputString = args[1];
+            JsonNode data = JsonArray.Parse(inputString);
+            string basepath = (string)data["path"];
+            int maxGenerations = (int)data["maxGenerations"];
+            int timeLimit = (int)data["timeLimit"];
+            float targetFitness = (float)data["targetFitness"];
+            int maxFunctionEvaluations = (int)data["maxFevals"];
+
+            bool[] criteriaStatus = { maxGenerations > 0, timeLimit > 0, targetFitness > 0.0f, maxFunctionEvaluations > 0 };
+            if (!(criteriaStatus[0] || criteriaStatus[1] || criteriaStatus[2] || criteriaStatus[3]))
+            {
+                Console.WriteLine("No valid stopping criteria was set!");
+                return;
+            }
+
+            bool keepMultiple = (bool)data["keepMultiple"];
+            string instance = (string)data["instanceName"];
+            string experimentName = (string)data["name"];
+
+            // legacy flags
+            int iteration = Int32.Parse(args[2]);
+            GA.SORT = true;
+            WorkerBenchmarkParser parser = new WorkerBenchmarkParser();
+            WorkerEncoding encoding = parser.ParseBenchmark(instance);
+            WorkerDecisionVariables variables = new WorkerDecisionVariables(encoding);
+            GAConfiguration config = new GAConfiguration(encoding, variables);
+            GA ga = new GA(config, true, encoding.Durations);
+            ga.SetStoppingCriteriaStatus(criteriaStatus[0], criteriaStatus[1], criteriaStatus[3], criteriaStatus[2]);
+            History gaResult = ga.Run(maxGenerations, timeLimit, targetFitness, maxFunctionEvaluations, keepMultiple);
+
+        }
+    }
+}
